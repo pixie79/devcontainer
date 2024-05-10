ea5509a58b33e5d44ac6959fe9d2bbce262760c1
Mark Olliver <mark@olliver.me.uk>
2024-05-10 10:34:47 +0100

    updates

--- src/image/.devcontainer/setup-devcontainer.sh
+++ src/image/.devcontainer/setup-devcontainer.sh
@@ -9,10 +9,26 @@ sudo locale-gen
 echo "PATH=${PWD}/.trunk/tools/:${HOME}/.local/bin:/home/codespace/venv/bin:$PATH" >>~/.zshrc
 echo "WORKSPACE=${PWD}" >>~/.zshrc
 
+export ARCH=$(dpkg --print-architecture)
+
 if [[ -f /home/codespace/requirements.txt ]]; then
     python3 -m venv /home/codespace/venv
     source /home/codespace/venv/bin/activate
     python3.11 -m pip --disable-pip-version-check --no-cache-dir install -r /home/codespace/requirements.txt
+fi
+
+if [[ -f /home/codespace/requirements-data.txt ]]; then
+    case "${DATA,,}" in
+    "true"|"t")
+        echo "Installing Python3 data tools."
+        python3 -m venv /home/codespace/venv
+        source /home/codespace/venv/bin/activate
+        python3.11 -m pip --disable-pip-version-check --no-cache-dir install -r /home/codespace/requirements-data.txt
+        ;;
+    *)
+        echo "Not installing Python3 data tools as DATA is not set to TRUE"
+        ;;
+    esac
 fi
 
 if [[ -d ${PWD}/.env ]]; then
@@ -34,15 +50,20 @@ if [[ -f ${PWD}/.trunk/trunk.yaml ]]; then
     /usr/local/bin/update-trunk-simlinks.sh
 fi
 
-if [[ -f ${PWD}/commitlint.config.js ]]; then
-    commitizen init cz-conventional-changelog --save-dev --save-exact --force
-fi
-
 if [[ -f ${PWD}/pyproject.toml ]]; then
     poetry config virtualenvs.in-project true
     poetry config virtualenvs.path ".venv"
     poetry install
 fi
 
+curl -L https://github.com/redpanda-data/redpanda/releases/latest/download/rpk-linux-${ARCH}.zip -o rpk-linux.zip
+unzip rpk-linux.zip
+mv rpk ~/.local/bin
+chmod +x ~/.local/bin/rpk
+rm rpk-linux.zip
+
+curl -L https://github.com/minamijoyo/tfupdate/releases/download/v0.8.2/tfupdate_0.8.2_linux_${ARCH}.tar.gz -o tfupdate.tar.gz
+tar zxvf tfupdate.tar.gz tfupdate
+mv tfupdate ~/.local/bin
+
 echo "please restart your ZSH Shell"
-echo "install rpk, tfupdate"
